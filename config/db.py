b02fed41f75d23318d3533b8cb3f71397d4b2346
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-12 22:19:26 +0530

    fastAPI fix 2

---
+++ config/db.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+from pydantic import BeforeValidator
+import motor.motor_asyncio
+
+import config.env as env
+
+
+settings = env.get_settings()
+
+MONGODB_URL: str = "mongodb+srv://{}:{}@{}/".format(
+    settings.DB_USER, settings.DB_PASSWORD, settings.DB_HOST
+).replace('"', "")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
+db = client.get_database("InterviewPerfect")
+questions_collection = db.get_collection("Questions")
+
+
+
+# Represents an ObjectId field in the database.
+# It will be represented as a ⁠ str ⁠ on the model so that it can be serialized to JSON.
+PyObjectId = Annotated[str, BeforeValidator(str)]
