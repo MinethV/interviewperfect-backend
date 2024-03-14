1e50bb58c50d436977a3e1ebb609be245b6a776b
Sasiru-Jayawardhana <sasiru.20210801@iit.ac.lk>
2024-03-14 14:52:33 +0530

    Update Operations Completed

--- CRUD-Application/routes/route.py
+++ CRUD-Application/routes/route.py
@@ -8,12 +8,18 @@ router = APIRouter()
 
 #Get request to get all the questions
 
-# @router.get("/questions")
+#Read the data from the database
 @router.get("/")
 async def get_all_questions():
     questions = list_serial(collection_name.find())
     return questions
 
+#Create a new question
 @router.post("/")
 async def create_question(question: Question):
     collection_name.insert_one(dict(question))
+
+#Update a question
+@router.get("/{id}")
+async def get_question_by_id(id:str, question: Question):
+    collection_name.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(question)})
