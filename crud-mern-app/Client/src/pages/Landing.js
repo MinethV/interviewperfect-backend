579041ea5a5e54d69e1bbbd6daa70952f534a868
Sasiru-Jayawardhana <sasiru.20210801@iit.ac.lk>
2024-03-22 21:59:05 +0530

    Margin fixing

--- crud-mern-app/Client/src/pages/Landing.js
+++ crud-mern-app/Client/src/pages/Landing.js
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import backgroundImage from "../images/background.png";
 import LandingImg from '../images/crudapp.png';
 
@@ -7,23 +6,23 @@ export const Landing = () => {
     const navigate = useNavigate();
 
     return (
-        <Container fluid className="min-vh-100" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-            <Row className="align-items-center">
-                <Col md={6} className="d-flex align-items-center">
-                    <img src={LandingImg} className="img-fluid mt-5 ms-5" alt="CRUD App" style={{width: '85%', height: 'auto'}} />
-                </Col>
-                <Col md={6} className="d-flex flex-column justify-content-center align-items-center me-5 me-md-0 ">
-                    <Card className="p-4 box rounded-5 col-lg-5 col-md-6 offset-md-3 col-sm-12 h-500 d-flex flex-column justify-content-center align-items-center" style={{ height: '500px', width: '500px', background: 'rgba(255,255,255,0.5)', '-webkit-backdrop-filter': 'blur(10px)', 'backdrop-filter': 'blur(10px)', border: '1px solid rgba(255,255,255,0.25)', borderRadius: '45px', marginRight: '250px' }}>
-                        <h1 className="lh-1 heading text-center  " style={{color: '#434445' , fontWeight: 'bold'}}>Database<br />
+        <div className="container-fluid min-vh-100" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <div className="row align-items-center">
+                <div className="col-md-6 d-flex align-items-center ms-auto">
+                    <img src={LandingImg} className="img-fluid ms-5" alt="CRUD App" style={{ width: '70%', height: 'auto', marginTop:'15vh' }} />
+                </div>
+                <div className="col-md-6 d-flex flex-column justify-content-center align-items-center me-auto me-md-0 mt-5 pt-3">
+                    <div className="p-4 box rounded-5 col-lg-5 col-md-6 offset-md-3 col-sm-12 h-500 d-flex flex-column justify-content-center align-items-center" style={{ height: '500px', width: '500px', background: 'rgba(255,255,255,0.5)', WebkitBackdropFilter: 'blur(10px)', backdropFilter: 'blur(10px)', border: '1px solid rgba(255,255,255,0.25)', borderRadius: '45px', marginRight: '250px' }}>
+                        <h1 className="lh-1 heading text-center" style={{ color: '#434445', fontWeight: 'bold' }}>Database<br />
                             Management<br />
                             System</h1><br /><br />
-                            <p className="fs-5 fw-light text-center" style={{color: '#616364',fontWeight: 'bold'}}>You can Create, Update, Search and <br />
+                        <p className="fs-5 fw-light text-center" style={{ color: '#616364', fontWeight: 'bold' }}>You can Create, Update, Search and <br />
                             Delete questions in this application<br />
                         </p><br /><br />
-                        <Button variant="primary" style={{backgroundColor: '#3288FF', ':hover': {backgroundColor: '#003366'}, borderRadius: '5rem', padding: '10px 30px', fontSize: '1.5rem'}} onClick={() => navigate('/read')}>Visit the DBMS</Button>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+                        <button className="btn btn-primary" style={{ backgroundColor: '#3288FF', borderRadius: '5rem', padding: '10px 30px', fontSize: '1.5rem' }} onClick={() => navigate('/read')}>Visit the DBMS</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
