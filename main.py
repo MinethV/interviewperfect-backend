93ac3a6c2fd457b9874e9c698eade52351fbaed9
oneli2002 <oneli.20221532@iit.ac.lk>
2024-03-22 23:35:16 +0530

    added model videos get request

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ settings = env.get_settings()
 async def home():
     return "Hello, World!"
 
+
 @app.get(
     "/questions/",
     response_description="List all questions",
@@ -58,6 +59,7 @@ async def get_software_engineering_questions():
         Questions=await db.questions_collection.find({"industry": "Software Engineering"}).to_list(5)
     )
 
+
 @app.get(
     "/questions/civilengineering",  # Corrected endpoint path
     response_description="List all questions related to Civil Engineering",
@@ -73,8 +75,65 @@ async def get_civil_engineering_questions():
     )
 
 
+@app.get(
+    "/modelvideos",
+    response_description="List all model videos",
+    response_model= modelVideos.ModelVideoCollection,
+    response_model_by_alias=False,
+)
+async def get_question():
+    """
+    List all of the questions in the database.
+
+    """
+    return modelVideos.ModelVideoCollection(
+        modelVideos=await db.modelvideo_collection.find().to_list(21)
+    )
+
+
+ @app.get(
+    "/modelvideos/softwareengineering",  
+    response_description="List all model videos",
+    response_model= modelVideos.ModelVideoCollection,
+    response_model_by_alias=False,
+)
+async def get_software_engineering_videos():
+    """
+    List all of the questions in the database.
+
+    """
+    return modelVideos.ModelVideoCollection(
+        modelVideos=await db.modelvideo_collection.find({'industry':'Software Engineering'}).to_list(2)
+    )   
  
 
+ @app.get(
+    "/modelvideos/civilengineering",  
+    response_description="List all model videos",
+    response_model= modelVideos.ModelVideoCollection,
+    response_model_by_alias=False,
+)
+async def get_civil_engineering_videos():
+    """
+    List all of the questions in the database.
+
+    """
+    return modelVideos.ModelVideoCollection(
+        modelVideos=await db.modelvideo_collection.find({'industry':'Civil Engineering'}).to_list(2)
+    )   
 
 
+ @app.get(
+    "/modelvideos/humanresources",  
+    response_description="List all model videos",
+    response_model= modelVideos.ModelVideoCollection,
+    response_model_by_alias=False,
+)
+async def get_human_resources_videos():
+    """
+    List all of the questions in the database.
 
+    """
+    return modelVideos.ModelVideoCollection(
+        modelVideos=await db.modelvideo_collection.find({'industry':'HR'}).to_list(2)
+    )   
