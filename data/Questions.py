d96c68bd8df6fb9192d4213c640ad3ff72b8d945
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-24 16:17:52 +0530

    Update Questions.py

--- data/Questions.py
+++ data/Questions.py
@@ -30,4 +30,4 @@ class QuestionCollection(BaseModel):
     This exists because providing a top-level array in a JSON response can be a [vulnerability](https://haacked.com/archive/2009/06/25/json-hijacking.aspx/)
     """
 
-    Questions: List[QuestionModel]
+    Questions: List[QuestionModel] 
