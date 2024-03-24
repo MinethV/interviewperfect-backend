572445a6ccf10d54e796c027c1d96a1a87c90f10
Mineth Vismitha <minethvismitha@gmail.com>
2024-03-24 16:17:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI, Body, status, HTTPException
 from bson import ObjectId
 from fastapi import FastAPI, HTTPException, Body, status
 from fastapi.middleware.cors import CORSMiddleware
+from Facial_Confident_Level.face import run_facial_confidence_detection
 
 import config.env as env
 import config.db as db
 from data import Questions
 from data import modelVideos
+
+import subprocess
 
 app = FastAPI()
 
@@ -91,6 +94,58 @@ async def get_question():
     )
 
  
+
+
+
+
+@app.post("/run_model")
+async def run_model():
+    try:
+        # Execute the IPython Notebook
+        process = subprocess.run(['jupyter', 'nbconvert', '--to', 'script', '--execute', 'Untitled3-4.ipynb'], capture_output=True, text=True)
+        if process.returncode == 0:
+            return {"success": True, "message": "Notebook executed successfully", "output": process.stdout}
+        else:
+            raise HTTPException(status_code=500, detail=f"Execution failed: {process.stderr}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Execution failed: {str(e)}")
+
+
+
+
+@app.get(
+    "/modelvideos/softwareengineering/technical",  
+    response_description="List all model videos",
+    response_model= modelVideos.ModelVideoCollection,
+    response_model_by_alias=False,
+)
+async def get_ui_ux_engineering_technical():
+    """
+    List all of the questions in the database.
+
+    """
+    return modelVideos.ModelVideoCollection(
+        modelVideos=await db.modelvideo_collection.find({ 
+                "$and": [ 
+                    {'industry':'Software Engineering'},
+                    {"question_type": "Technical"}
+                ]
+            }
+        ).to_list(10)
+    )
+
+
+@app.get("/predict")
+async def predict():
+    run_facial_confidence_detection()
+    return {"message": "Facial confidence detection completed."}
+
+
+
+
+
+
+
 
 
 
